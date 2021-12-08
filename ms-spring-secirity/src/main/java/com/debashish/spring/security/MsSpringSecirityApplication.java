117c19e96c7e0268bf3a1c748cc6054d54d76261
debashishbarik <debashish.barik@hotmail.com>
2021-12-08 22:38:24 +0530

    inital commit

---
+++ ms-spring-secirity/src/main/java/com/debashish/spring/security/MsSpringSecirityApplication.java
@@ -0,0 +1,13 @@
+package com.debashish.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringSecirityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringSecirityApplication.class, args);
+	}
+
+}
---
+++ ms-spring-secirity/src/test/java/com/debashish/spring/security/MsSpringSecirityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debashish.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringSecirityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
